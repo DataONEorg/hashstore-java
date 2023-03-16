7ae98c48a13b6158f2cf0aaf3410b3eae1f731f6
Matt Jones <gitcode@magisa.org>
2023-03-15 17:38:19 -0800

    Initial project structure for `hashstore` in Java.

---
+++ src/main/java/org/dataone/hashstore/HashStore.java
@@ -0,0 +1,13 @@
+package org.dataone.hashstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class HashStore 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/dataone/hashstore/HashStoreTest.java
@@ -0,0 +1,20 @@
+package org.dataone.hashstore;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HashStoreTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
