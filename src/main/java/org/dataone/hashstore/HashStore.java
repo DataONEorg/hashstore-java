6144325ed788eb3d0afd15fe180c7f2f404670c5
Dou Mok <doumok@Dous-MacBook-Pro.local>
2023-03-21 12:49:56 -0700

    Add HashStore constructor

--- src/main/java/org/dataone/hashstore/HashStore.java
+++ src/main/java/org/dataone/hashstore/HashStore.java
@@ -1,10 +1,18 @@
 package org.dataone.hashstore;
 
 /**
- * Hello world!
- *
+ * HashStore is a file store system that uses the object's hex digest value as
+ * the file address and stores the respective sysmeta documents in a separate
+ * directory using the given identifier's (pid) hex digest value.
  */
 public class HashStore {
+    private String sysmetaNameSpace;
+
+    public HashStore(byte depth, byte width, String namespace) {
+        this.sysmetaNameSpace = namespace;
+        // TODO: Initialize File Store
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
