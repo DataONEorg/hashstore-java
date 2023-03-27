3ba29ef6269df4d756fa63f6c9066d7ad7750b4e
Dou Mok <doumok@Dous-MacBook-Pro.local>
2023-03-27 12:22:31 -0700

    Revise HashFileStore to create default store directory when path provided is null

--- src/main/java/org/dataone/hashstore/hashfs/HashFileStore.java
+++ src/main/java/org/dataone/hashstore/hashfs/HashFileStore.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * HashFileStore handles IO operations for HashStore
+ */
 public class HashFileStore {
     private int directoryDepth;
     private int directoryWidth;
@@ -13,6 +16,13 @@ public class HashFileStore {
 
     /**
      * Constructor to initialize HashStore fields and object store directory
+     * 
+     * @param depth
+     * @param width
+     * @param algorithm
+     * @param storeDirectory Full file path (ex. /usr/org/objects)
+     * @throws IllegalArgumentException
+     * @throws IOException
      */
     public HashFileStore(int depth, int width, String algorithm, String storeDirectory)
             throws IllegalArgumentException, IOException {
@@ -23,12 +33,21 @@ public class HashFileStore {
         if (algorithm == null || algorithm.isEmpty()) {
             throw new IllegalArgumentException("Algorithm cannot be null or empty.");
         }
+        // TODO: Handle when algorithm is not "sha256"
 
         this.directoryDepth = depth;
         this.directoryWidth = width;
         this.algorithm = algorithm;
-        this.rootDirectory = System.getProperty("user.dir");
-        Path objectStoreDirectory = Paths.get(rootDirectory).resolve(storeDirectory);
+
+        // If no path provided, create default path with user.dir root + /HashFileStore
+        Path objectStoreDirectory;
+        if (storeDirectory == null) {
+            this.rootDirectory = System.getProperty("user.dir");
+            String objectPath = "HashFileStore";
+            objectStoreDirectory = Paths.get(rootDirectory).resolve(objectPath);
+        } else {
+            objectStoreDirectory = Paths.get(storeDirectory);
+        }
 
         // Create store directory
         try {
