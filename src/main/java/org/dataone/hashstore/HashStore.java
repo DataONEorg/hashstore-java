9544949584f55e1b5a4ed64fa88e95b599a2c52a
Dou Mok <doumok@Dous-MacBook-Pro.local>
2023-03-27 12:13:16 -0700

    Update documentation for HashStore

--- src/main/java/org/dataone/hashstore/HashStore.java
+++ src/main/java/org/dataone/hashstore/HashStore.java
@@ -5,9 +5,8 @@ import java.io.IOException;
 import org.dataone.hashstore.hashfs.HashFileStore;
 
 /**
- * HashStore is a file store system that uses the object's hex digest value as
- * the file address and stores the respective sysmeta documents in a separate
- * directory using the given identifier's (pid) hex digest value.
+ * HashStore is a file store system/content-addressable file manager that uses
+ * the object's hex digest value as the file address
  */
 public class HashStore {
     private String sysmetaNameSpace = "http://ns.dataone.org/service/types/v2.0";
@@ -15,6 +14,13 @@ public class HashStore {
     private int depth = 3;
     private int width = 2;
 
+    /**
+     * Default constructor for HashStore
+     * 
+     * @param storeDirectory Full file path (ex. /usr/org/metacat/objects)
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
     public HashStore(String storeDirectory)
             throws IllegalArgumentException, IOException {
         try {
