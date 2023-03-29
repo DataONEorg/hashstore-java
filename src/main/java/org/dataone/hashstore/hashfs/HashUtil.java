0a9d83b25aa339eb54bf31bcfa9e94b43eaaf4c8
Dou Mok <doumok@Dous-MacBook-Pro.local>
2023-03-29 10:17:53 -0700

    Add new method to Hashutil (writeToTmpFileAndGenerateChecksums) and junit test

--- src/main/java/org/dataone/hashstore/hashfs/HashUtil.java
+++ src/main/java/org/dataone/hashstore/hashfs/HashUtil.java
@@ -1,7 +1,15 @@
 package org.dataone.hashstore.hashfs;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
 
 /**
  * HashUtil provides utility methods for HashFileStore
@@ -28,4 +36,61 @@ public class HashUtil {
         // TODO: Log - newFile.getCanonicalPath());
         return newFile;
     }
+
+    /**
+     * Write the input stream into a given file (tmpFile) and return a HashMap of
+     * the default algorithms and their respective hex digests
+     * 
+     * @param tmpFile             File into which the stream will be written to
+     * @param dataStream          Source data stream
+     * @param additionalAlgorithm Optional additional algoritm to generate
+     */
+    public Map<String, String> writeToTmpFileAndGenerateChecksums(File tmpFile, InputStream dataStream,
+            String additionalAlgorithm) throws NoSuchAlgorithmException, IOException {
+        // TODO: Handle additionalAlgorithm when not null
+        Map<String, String> hexDigests = new HashMap<>();
+
+        FileOutputStream os = new FileOutputStream(tmpFile);
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        MessageDigest sha384 = MessageDigest.getInstance("SHA-384");
+        MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+
+        try {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = dataStream.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+                md5.update(buffer, 0, bytesRead);
+                sha1.update(buffer, 0, bytesRead);
+                sha256.update(buffer, 0, bytesRead);
+                sha384.update(buffer, 0, bytesRead);
+                sha512.update(buffer, 0, bytesRead);
+            }
+        } finally {
+            if (os != null) {
+                try {
+                    os.flush();
+                    os.close();
+                } catch (Exception e) {
+                    // TODO: Log exception
+                }
+            }
+        }
+
+        String md5Checksum = DatatypeConverter.printHexBinary(md5.digest()).toLowerCase();
+        String sha1Digest = DatatypeConverter.printHexBinary(sha1.digest()).toLowerCase();
+        String sha256Digest = DatatypeConverter.printHexBinary(sha256.digest()).toLowerCase();
+        String sha384Digest = DatatypeConverter.printHexBinary(sha384.digest()).toLowerCase();
+        String sha512Digest = DatatypeConverter.printHexBinary(sha512.digest()).toLowerCase();
+        hexDigests.put("md5", md5Checksum);
+        hexDigests.put("sha1", sha1Digest);
+        hexDigests.put("sha256", sha256Digest);
+        hexDigests.put("sha384", sha384Digest);
+        hexDigests.put("sha512", sha512Digest);
+
+        return hexDigests;
+    }
+
 }
--- src/test/java/org/dataone/hashstore/HashUtilTest.java
+++ src/test/java/org/dataone/hashstore/HashUtilTest.java
@@ -1,10 +1,18 @@
 package org.dataone.hashstore;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 import org.dataone.hashstore.hashfs.HashUtil;
 import org.junit.Rule;
@@ -16,14 +24,10 @@ import org.junit.rules.TemporaryFolder;
  */
 public class HashUtilTest {
 
-    @Rule
-    public TemporaryFolder tempFolder = new TemporaryFolder();
-
-    /**
-     * Confirm that a temporary file has been generated.
+    /*
+     * Non-test method using HashUtil class to generate a temp file
      */
-    @Test
-    public void testCreateTemporaryFile() {
+    public File generateTemporaryFile() {
         String prefix = "testfile";
         File directory = tempFolder.getRoot();
         File newFile = null;
@@ -33,6 +37,60 @@ public class HashUtilTest {
         } catch (IOException e) {
             fail("IOException encountered: " + e.getMessage());
         }
-        assertTrue(newFile.exists());
+        return newFile;
+    }
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    /**
+     * Confirm that a temporary file has been generated.
+     */
+    @Test
+    public void testCreateTemporaryFile() {
+        File newTmpFile = generateTemporaryFile();
+        assertTrue(newTmpFile.exists());
+    }
+
+    /**
+     * Check that the temporary file is not empty and that a list of
+     * checksums are generated.
+     */
+    @Test
+    public void testWriteToTempFileAndGenerateChecksums() {
+        File newTmpFile = generateTemporaryFile();
+
+        // Get test file
+        Path testdataDirectory = Paths.get("src/test/java/org/dataone/hashstore", "testdata", "jtao.1700.1");
+        String testdataAbsolutePath = testdataDirectory.toFile().getAbsolutePath();
+        File testDataFile = new File(testdataAbsolutePath);
+
+        try {
+            InputStream dataStream = new FileInputStream(testDataFile);
+            HashUtil hsil = new HashUtil();
+            Map<String, String> hexDigests = hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream, null);
+
+            // Validate checksum values
+            String md5 = "f4ea2d07db950873462a064937197b0f";
+            String sha1 = "3d25436c4490b08a2646e283dada5c60e5c0539d";
+            String sha256 = "94f9b6c88f1f458e410c30c351c6384ea42ac1b5ee1f8430d3e365e43b78a38a";
+            String sha384 = "a204678330fcdc04980c9327d4e5daf01ab7541e8a351d49a7e9c5005439dce749ada39c4c35f573dd7d307cca11bea8";
+            String sha512 = "bf9e7f4d4e66bd082817d87659d1d57c2220c376cd032ed97cadd481cf40d78dd479cbed14d34d98bae8cebc603b40c633d088751f07155a94468aa59e2ad109";
+            assertEquals(md5, hexDigests.get("md5"));
+            assertEquals(sha1, hexDigests.get("sha1"));
+            assertEquals(sha256, hexDigests.get("sha256"));
+            assertEquals(sha384, hexDigests.get("sha384"));
+            assertEquals(sha512, hexDigests.get("sha512"));
+
+            long testDataFileSize = Files.size(testDataFile.toPath());
+            Path tmpFilePath = newTmpFile.toPath();
+            long tmpFileSize = Files.size(tmpFilePath);
+            assertEquals(testDataFileSize, tmpFileSize);
+
+        } catch (NoSuchAlgorithmException e) {
+            fail("NoSuchAlgorithmExceptionJava: " + e.getMessage());
+        } catch (IOException e) {
+            fail("IOException: " + e.getMessage());
+        }
     }
 }
