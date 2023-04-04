785f3cf93d263549a502cb9c232c03b63c8d7e3a
Dou Mok <douming.mok@gmail.com>
2023-04-04 13:32:36 -0700

    Remove unused import in HashFileStoreTest

--- src/test/java/org/dataone/hashstore/HashFileStoreTest.java
+++ src/test/java/org/dataone/hashstore/HashFileStoreTest.java
@@ -11,7 +11,6 @@ import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
