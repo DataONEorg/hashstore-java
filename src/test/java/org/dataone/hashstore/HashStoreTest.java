4cb26b1ca9eba38fe67bccc1733934bc6046fa77
Dou Mok <douming.mok@gmail.com>
2023-04-17 14:34:08 -0700

    Add Hashstore test to check constructor initialization

--- src/test/java/org/dataone/hashstore/HashStoreTest.java
+++ src/test/java/org/dataone/hashstore/HashStoreTest.java
@@ -1,18 +1,51 @@
 package org.dataone.hashstore;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
- * Unit test for simple App.
+ * Unit tests for HashStore-java
  */
 public class HashStoreTest {
+    public HashStore hsj;
+    public Path rootDirectory;
+    public String rootString;
+    public String rootStringFull;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Before
+    public void initializeHashStore() {
+        this.rootDirectory = tempFolder.getRoot().toPath();
+        this.rootString = rootDirectory.toString();
+        this.rootStringFull = rootString + "/metacat";
+        try {
+            this.hsj = new HashStore(rootStringFull);
+        } catch (IOException e) {
+            fail("IOException encountered: " + e.getMessage());
+        }
+    }
+
     /**
-     * Rigorous Test :-)
+     * Check that storeDirectory is created
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testHashStoreConstructor() {
+        Path checkStoreObjPath = Paths.get(this.rootStringFull).resolve("objects");
+        assertTrue(Files.exists(checkStoreObjPath));
+
+        Path checkStoreObjTmpPath = Paths.get(this.rootStringFull).resolve("objects/tmp");
+        assertTrue(Files.exists(checkStoreObjTmpPath));
     }
 }
