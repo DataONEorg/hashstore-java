dbe62ba58cc0be580a74057e45e4e18e23d7b364
Dou Mok <douming.mok@gmail.com>
2023-04-19 15:01:39 -0700

    Refactor HashUtil tests

--- src/test/java/org/dataone/hashstore/hashfs/HashUtilTest.java
+++ src/test/java/org/dataone/hashstore/hashfs/HashUtilTest.java
@@ -25,20 +25,16 @@ import org.junit.rules.TemporaryFolder;
  */
 public class HashUtilTest {
     public TestDataHarness testData = new TestDataHarness();
+    public HashUtil hsil = new HashUtil();
 
     /*
      * Non-test method using HashUtil class to generate a temp file
      */
-    public File generateTemporaryFile() {
+    public File generateTemporaryFile() throws Exception {
         String prefix = "testfile";
         File directory = tempFolder.getRoot();
         File newFile = null;
-        try {
-            HashUtil hsil = new HashUtil();
-            newFile = hsil.generateTmpFile(prefix, directory);
-        } catch (IOException e) {
-            fail("IOException encountered: " + e.getMessage());
-        }
+        newFile = this.hsil.generateTmpFile(prefix, directory);
         return newFile;
     }
 
@@ -49,7 +45,7 @@ public class HashUtilTest {
      * Confirm that a temporary file has been generated.
      */
     @Test
-    public void testCreateTemporaryFile() {
+    public void testGenerateTemporaryFile() throws Exception {
         File newTmpFile = generateTemporaryFile();
         assertTrue(newTmpFile.exists());
     }
@@ -59,53 +55,59 @@ public class HashUtilTest {
      */
     @Test
     public void testShardHexDigest() {
-        HashUtil hsil = new HashUtil();
-        String shardedPath = hsil.shard(3, 2, "94f9b6c88f1f458e410c30c351c6384ea42ac1b5ee1f8430d3e365e43b78a38a");
+        String shardedPath = this.hsil.shard(3, 2, "94f9b6c88f1f458e410c30c351c6384ea42ac1b5ee1f8430d3e365e43b78a38a");
         String shardedPathExpected = "/94/f9/b6/c88f1f458e410c30c351c6384ea42ac1b5ee1f8430d3e365e43b78a38a";
         assertEquals(shardedPath, shardedPathExpected);
     }
 
     /**
-     * Check algorithm support
+     * Check algorithm support for supported algorithm
      */
     @Test
-    public void testValidateAlgorithm() {
-        HashUtil hsil = new HashUtil();
-
+    public void testValidateAlgorithmSupported() {
         String md2 = "MD2";
-        boolean supported = hsil.validateAlgorithm(md2);
+        boolean supported = this.hsil.validateAlgorithm(md2);
         assertTrue(supported);
+    }
 
+    /**
+     * Check algorithm support for unsupported algorithm
+     */
+    @Test
+    public void testValidateAlgorithmNotSupported() {
         String sm3 = "SM3";
-        boolean not_supported = hsil.validateAlgorithm(sm3);
+        boolean not_supported = this.hsil.validateAlgorithm(sm3);
         assertFalse(not_supported);
+    }
 
+    /**
+     * Check algorithm support for unsupported algorithm
+     */
+    @Test
+    public void testValidateAlgorithmNotSupportedLowerCase() {
         // Must match string to reduce complexity, no string formatting
         String md2_lowercase = "md2";
-        boolean lowercase_not_supported = hsil.validateAlgorithm(md2_lowercase);
+        boolean lowercase_not_supported = this.hsil.validateAlgorithm(md2_lowercase);
         assertFalse(lowercase_not_supported);
     }
 
+    /**
+     * Check for correct hex digest value
+     */
     @Test
-    public void testGetHexDigest() {
-        HashUtil hsil = new HashUtil();
-        try {
-            for (String pid : this.testData.pidList) {
-                String abIdDigest = hsil.getHexDigest(pid, "SHA-256");
-                String abIdTestData = this.testData.pidData.get(pid).get("s_cid");
-                assertEquals(abIdDigest, abIdTestData);
-            }
-        } catch (NoSuchAlgorithmException nsae) {
-            fail("NoSuchAlgorithmException: " + nsae.getMessage());
+    public void testGetHexDigest() throws Exception {
+        for (String pid : this.testData.pidList) {
+            String abIdDigest = this.hsil.getHexDigest(pid, "SHA-256");
+            String abIdTestData = this.testData.pidData.get(pid).get("s_cid");
+            assertEquals(abIdDigest, abIdTestData);
         }
     }
 
     /**
-     * Check that the temporary file is not empty and that a list of
-     * checksums are generated.
+     * Check that checksums are generated.
      */
     @Test
-    public void testWriteToTempFileAndGenerateChecksums() {
+    public void testWriteToTempFileAndGenerateChecksums() throws Exception {
         for (String pid : this.testData.pidList) {
             File newTmpFile = generateTemporaryFile();
             String pidFormatted = pid.replace("/", "_");
@@ -115,41 +117,79 @@ public class HashUtilTest {
             String testdataAbsolutePath = testdataDirectory.toFile().getAbsolutePath();
             File testDataFile = new File(testdataAbsolutePath);
 
-            try {
-                // Extra algo to calculate - MD2
-                String addAlgo = "MD2";
+            // Extra algo to calculate - MD2
+            String addAlgo = "MD2";
 
-                InputStream dataStream = new FileInputStream(testDataFile);
-                HashUtil hsil = new HashUtil();
-                Map<String, String> hexDigests = hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
-                        addAlgo);
+            InputStream dataStream = new FileInputStream(testDataFile);
+            Map<String, String> hexDigests = this.hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
+                    addAlgo);
 
-                // Validate checksum values
-                String md5 = this.testData.pidData.get(pid).get("md5");
-                String sha1 = this.testData.pidData.get(pid).get("sha1");
-                String sha256 = this.testData.pidData.get(pid).get("sha256");
-                String sha384 = this.testData.pidData.get(pid).get("sha384");
-                String sha512 = this.testData.pidData.get(pid).get("sha512");
-                assertEquals(md5, hexDigests.get("MD5"));
-                assertEquals(sha1, hexDigests.get("SHA-1"));
-                assertEquals(sha256, hexDigests.get("SHA-256"));
-                assertEquals(sha384, hexDigests.get("SHA-384"));
-                assertEquals(sha512, hexDigests.get("SHA-512"));
+            // Validate checksum values
+            String md5 = this.testData.pidData.get(pid).get("md5");
+            String sha1 = this.testData.pidData.get(pid).get("sha1");
+            String sha256 = this.testData.pidData.get(pid).get("sha256");
+            String sha384 = this.testData.pidData.get(pid).get("sha384");
+            String sha512 = this.testData.pidData.get(pid).get("sha512");
+            assertEquals(md5, hexDigests.get("MD5"));
+            assertEquals(sha1, hexDigests.get("SHA-1"));
+            assertEquals(sha256, hexDigests.get("SHA-256"));
+            assertEquals(sha384, hexDigests.get("SHA-384"));
+            assertEquals(sha512, hexDigests.get("SHA-512"));
+        }
+    }
 
-                // Validate additional algorithm
-                String md2 = this.testData.pidData.get(pid).get("md2");
-                assertEquals(md2, hexDigests.get("MD2"));
+    /**
+     * Check that additional algorithm is generated and correct
+     */
+    @Test
+    public void testWriteToTempFileAndGenerateChecksumsAddAlgo() throws Exception {
+        for (String pid : this.testData.pidList) {
+            File newTmpFile = generateTemporaryFile();
+            String pidFormatted = pid.replace("/", "_");
 
-                long testDataFileSize = Files.size(testDataFile.toPath());
-                Path tmpFilePath = newTmpFile.toPath();
-                long tmpFileSize = Files.size(tmpFilePath);
-                assertEquals(testDataFileSize, tmpFileSize);
+            // Get test file
+            Path testdataDirectory = Paths.get("src/test/java/org/dataone/hashstore", "testdata", pidFormatted);
+            String testdataAbsolutePath = testdataDirectory.toFile().getAbsolutePath();
+            File testDataFile = new File(testdataAbsolutePath);
 
-            } catch (NoSuchAlgorithmException e) {
-                fail("NoSuchAlgorithmExceptionJava: " + e.getMessage());
-            } catch (IOException e) {
-                fail("IOException: " + e.getMessage());
-            }
+            // Extra algo to calculate - MD2
+            String addAlgo = "MD2";
+
+            InputStream dataStream = new FileInputStream(testDataFile);
+            Map<String, String> hexDigests = this.hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
+                    addAlgo);
+
+            // Validate additional algorithm
+            String md2 = this.testData.pidData.get(pid).get("md2");
+            assertEquals(md2, hexDigests.get("MD2"));
+        }
+    }
+
+    /**
+     * Check that the temporary file is not empty
+     */
+    @Test
+    public void testWriteToTempFileAndGenerateChecksumsTmpFileSize() throws Exception {
+        for (String pid : this.testData.pidList) {
+            File newTmpFile = generateTemporaryFile();
+            String pidFormatted = pid.replace("/", "_");
+
+            // Get test file
+            Path testdataDirectory = Paths.get("src/test/java/org/dataone/hashstore", "testdata", pidFormatted);
+            String testdataAbsolutePath = testdataDirectory.toFile().getAbsolutePath();
+            File testDataFile = new File(testdataAbsolutePath);
+
+            // Extra algo to calculate - MD2
+            String addAlgo = "MD2";
+
+            InputStream dataStream = new FileInputStream(testDataFile);
+            Map<String, String> hexDigests = this.hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
+                    addAlgo);
+
+            long testDataFileSize = Files.size(testDataFile.toPath());
+            Path tmpFilePath = newTmpFile.toPath();
+            long tmpFileSize = Files.size(tmpFilePath);
+            assertEquals(testDataFileSize, tmpFileSize);
         }
     }
 
@@ -157,7 +197,7 @@ public class HashUtilTest {
      * Check that exception is thrown when unsupported algorithm supplied
      */
     @Test(expected = IllegalArgumentException.class)
-    public void testWriteToTempFileAndGenerateChecksumsInvalidAlgo() {
+    public void testWriteToTempFileAndGenerateChecksumsInvalidAlgo() throws Exception {
         for (String pid : this.testData.pidList) {
             File newTmpFile = generateTemporaryFile();
             String pidFormatted = pid.replace("/", "_");
@@ -167,20 +207,12 @@ public class HashUtilTest {
             String testdataAbsolutePath = testdataDirectory.toFile().getAbsolutePath();
             File testDataFile = new File(testdataAbsolutePath);
 
-            try {
-                // Extra algo to calculate - MD2
-                String addAlgo = "SM2";
+            // Extra algo to calculate - MD2
+            String addAlgo = "SM2";
 
-                InputStream dataStream = new FileInputStream(testDataFile);
-                HashUtil hsil = new HashUtil();
-                Map<String, String> hexDigests = hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
-                        addAlgo);
-
-            } catch (NoSuchAlgorithmException e) {
-                fail("NoSuchAlgorithmExceptionJava: " + e.getMessage());
-            } catch (IOException e) {
-                fail("IOException: " + e.getMessage());
-            }
+            InputStream dataStream = new FileInputStream(testDataFile);
+            Map<String, String> hexDigests = this.hsil.writeToTmpFileAndGenerateChecksums(newTmpFile, dataStream,
+                    addAlgo);
         }
     }
 
@@ -188,18 +220,12 @@ public class HashUtilTest {
      * Confirm that object has moved
      */
     @Test
-    public void testMove() {
-        HashUtil hsil = new HashUtil();
-
+    public void testMove() throws Exception {
         File newTmpFile = generateTemporaryFile();
         String targetString = tempFolder.getRoot().toString() + "/testmove/test_tmp_object.tmp";
         File targetFile = new File(targetString);
 
-        try {
-            hsil.move(newTmpFile, targetFile);
-            assertTrue(targetFile.exists());
-        } catch (IOException e) {
-            fail("IOException: " + e.getMessage());
-        }
+        this.hsil.move(newTmpFile, targetFile);
+        assertTrue(targetFile.exists());
     }
 }
