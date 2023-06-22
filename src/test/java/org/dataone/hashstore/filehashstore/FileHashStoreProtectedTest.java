4e995ae72d132880a4ef5a9357f32f7d0d81a5a7
Dou Mok <douming.mok@gmail.com>
2023-06-22 16:41:04 -0700

    Fix bug in junit test for 'writeTo...Checksums()' where assertion for additional algorithm checksum value was omitted

--- src/test/java/org/dataone/hashstore/filehashstore/FileHashStoreProtectedTest.java
+++ src/test/java/org/dataone/hashstore/filehashstore/FileHashStoreProtectedTest.java
@@ -477,11 +477,13 @@ public class FileHashStoreProtectedTest {
                     addAlgo);
 
             // Validate checksum values
+            String md2 = testData.pidData.get(pid).get("md2");
             String md5 = testData.pidData.get(pid).get("md5");
             String sha1 = testData.pidData.get(pid).get("sha1");
             String sha256 = testData.pidData.get(pid).get("sha256");
             String sha384 = testData.pidData.get(pid).get("sha384");
             String sha512 = testData.pidData.get(pid).get("sha512");
+            assertEquals(md2, hexDigests.get("MD2"));
             assertEquals(md5, hexDigests.get("MD5"));
             assertEquals(sha1, hexDigests.get("SHA-1"));
             assertEquals(sha256, hexDigests.get("SHA-256"));
