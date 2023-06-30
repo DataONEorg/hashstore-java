b05906c528636269af86fa4adc0bf51271ea2c52
Dou Mok <douming.mok@gmail.com>
2023-06-30 09:28:11 -0700

    Cleanup 'HashAddress' javadocs

--- src/main/java/org/dataone/hashstore/HashAddress.java
+++ src/main/java/org/dataone/hashstore/HashAddress.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * HashAddress is a class that models a unique identifier for a file in the
  * Hashstore. It encapsulates information about the file's name, path, and
  * associated hash digest values. By using HashAddress objects, client code can
- * easily locate, retrieve, and modify files in the Hashstore without needing to
+ * easily locate, retrieve, and modify files in the HashStore without needing to
  * know the underlying file system details.
  */
 public class HashAddress {
@@ -20,17 +20,16 @@ public class HashAddress {
     /**
      * Creates a new instance of HashAddress with the given properties.
      *
-     * @param id          the unique identifier for the file
-     * @param relPath     the relative path of the file within the hash store
-     * @param absPath     the absolute path of the file on the local file system
-     * @param isDuplicate a flag indicating if the file is a duplicate of an
+     * @param id          Unique identifier for the file
+     * @param relPath     Relative path of the file within the hash store
+     * @param absPath     Absolute path of the file on the local file system
+     * @param isDuplicate Flag indicating if the file is a duplicate of an
      *                    existing file
-     * @param hexDigests  a map of hash algorithm names to their hex-encoded
+     * @param hexDigests  A map of hash algorithm names to their hex-encoded
      *                    digest values for the file
      */
     public HashAddress(String id, String relPath, Path absPath, boolean isDuplicate,
             Map<String, String> hexDigests) {
-        // Constructor implementation
         this.id = id;
         this.relPath = relPath;
         this.absPath = absPath;
@@ -50,7 +49,7 @@ public class HashAddress {
     /**
      * Return the relative path to the file
      * 
-     * @return relative path
+     * @return relPath
      */
     public String getRelPath() {
         return relPath;
@@ -59,7 +58,7 @@ public class HashAddress {
     /**
      * Return the absolute path to the file
      * 
-     * @return absolute path
+     * @return absPath
      */
     public Path getAbsPath() {
         return absPath;
@@ -68,7 +67,7 @@ public class HashAddress {
     /**
      * Return the flag of whether a file is a duplicate or not
      * 
-     * @return true if the file is not a duplicate
+     * @return True if the file is not a duplicate, false otherwise
      */
     public boolean getIsDuplicate() {
         return isDuplicate;
@@ -77,7 +76,7 @@ public class HashAddress {
     /**
      * Return a map of hex digests
      * 
-     * @return hex digest map
+     * @return hexDigests
      */
     public Map<String, String> getHexDigests() {
         return hexDigests;
