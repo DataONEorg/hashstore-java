44a55988d9cf90f7e5849219acf4ce841d60205b
Dou Mok <douming.mok@gmail.com>
2024-06-18 12:38:12 -0700

    Fix incorrect extends exception type in 'NonMatchingChecksumException' and 'NonMatchingChecksumException' class

--- src/main/java/org/dataone/hashstore/exceptions/NonMatchingChecksumException.java
+++ src/main/java/org/dataone/hashstore/exceptions/NonMatchingChecksumException.java
@@ -2,7 +2,7 @@ package org.dataone.hashstore.exceptions;
 
 import java.io.IOException;
 
-public class NonMatchingChecksumException extends IOException {
+public class NonMatchingChecksumException extends IllegalArgumentException {
 
     public NonMatchingChecksumException(String message) {
         super(message);
--- src/main/java/org/dataone/hashstore/exceptions/NonMatchingObjSizeException.java
+++ src/main/java/org/dataone/hashstore/exceptions/NonMatchingObjSizeException.java
@@ -2,7 +2,7 @@ package org.dataone.hashstore.exceptions;
 
 import java.io.IOException;
 
-public class NonMatchingObjSizeException extends IOException {
+public class NonMatchingObjSizeException extends IllegalArgumentException {
 
     public NonMatchingObjSizeException(String message) {
         super(message);
