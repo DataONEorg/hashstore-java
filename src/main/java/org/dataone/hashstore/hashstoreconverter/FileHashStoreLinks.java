24315f0de72dcaf6dd2022693c96fdaf502608c1
Dou Mok <douming.mok@gmail.com>
2024-07-24 16:12:05 -0700

    Create new package 'hashstoreconverter' and add new class 'FileHashStoreLinks' with basic constructor

---
+++ src/main/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinks.java
@@ -0,0 +1,11 @@
+package org.dataone.hashstore.hashstoreconverter;
+
+import java.util.Properties;
+
+public class FileHashStoreLinks {
+
+    public FileHashStoreLinks(Properties hashstoreProperties) throws Exception {
+        super();
+    }
+
+}
