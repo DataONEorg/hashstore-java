713bda82d5af53eb5e134d61f526267f56cb1a0c
Dou Mok <douming.mok@gmail.com>
2024-07-24 17:10:54 -0700

    Add logging module to 'FileHashStoreLinks'

--- src/main/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinks.java
+++ src/main/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinks.java
@@ -1,5 +1,7 @@
 package org.dataone.hashstore.hashstoreconverter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.dataone.hashstore.filehashstore.FileHashStore;
 
 import java.io.IOException;
@@ -8,9 +10,12 @@ import java.util.Properties;
 
 public class FileHashStoreLinks extends FileHashStore {
 
+    private static final Log logFileHashStoreLinks = LogFactory.getLog(FileHashStore.class);
+
     public FileHashStoreLinks(Properties hashstoreProperties) throws IllegalArgumentException,
         IOException, NoSuchAlgorithmException {
         super(hashstoreProperties);
+        logFileHashStoreLinks.info("FileHashStoreLinks initialized");
     }
 
 }
