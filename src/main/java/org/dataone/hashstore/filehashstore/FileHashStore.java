e3b40164f79abd2c5deb5ce147d883c6005506eb
Dou Mok <douming.mok@gmail.com>
2024-07-26 12:01:25 -0700

    Change access modifier to public for 'enum DefaultHashAlgorithms' in 'FileHashStore', and add new method 'generateChecksums' in 'FileHashStoreLinks'

--- src/main/java/org/dataone/hashstore/filehashstore/FileHashStore.java
+++ src/main/java/org/dataone/hashstore/filehashstore/FileHashStore.java
@@ -94,7 +94,7 @@ public class FileHashStore implements HashStore {
         }
     }
 
-    enum DefaultHashAlgorithms {
+    public enum DefaultHashAlgorithms {
         MD5("MD5"), SHA_1("SHA-1"), SHA_256("SHA-256"), SHA_384("SHA-384"), SHA_512("SHA-512");
 
         final String algoName;
--- src/main/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinks.java
+++ src/main/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinks.java
@@ -2,10 +2,25 @@ package org.dataone.hashstore.hashstoreconverter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.dataone.hashstore.ObjectMetadata;
+import org.dataone.hashstore.exceptions.PidRefsFileExistsException;
 import org.dataone.hashstore.filehashstore.FileHashStore;
+import org.dataone.hashstore.filehashstore.FileHashStoreUtility;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.AtomicMoveNotSupportedException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class FileHashStoreLinks extends FileHashStore {
@@ -16,6 +31,115 @@ public class FileHashStoreLinks extends FileHashStore {
         IOException, NoSuchAlgorithmException {
         super(hashstoreProperties);
         logFileHashStoreLinks.info("FileHashStoreLinks initialized");
+    }
+
+    /**
+     * Write the input stream into a given file (tmpFile) and return a HashMap consisting of
+     * algorithms and their respective hex digests. If an additional algorithm is supplied and
+     * supported, it and its checksum value will be included in the hex digests map. Default
+     * algorithms: MD5, SHA-1, SHA-256, SHA-384, SHA-512
+     *
+     * @param tmpFile             file to write input stream data into
+     * @param dataStream          input stream of data to store
+     * @param additionalAlgorithm additional algorithm to include in hex digest map
+     * @param checksumAlgorithm   checksum algorithm to calculate hex digest for to verifying
+     *                            object
+     * @return A map containing the hex digests of the default algorithms
+     * @throws NoSuchAlgorithmException Unable to generate new instance of supplied algorithm
+     * @throws IOException              Issue with writing file from InputStream
+     * @throws SecurityException        Unable to write to tmpFile
+     * @throws FileNotFoundException    tmpFile cannot be found
+     */
+    protected Map<String, String> generateChecksums(InputStream dataStream,
+                                                  String additionalAlgorithm, String checksumAlgorithm
+    ) throws NoSuchAlgorithmException, IOException, SecurityException {
+        // Determine whether to calculate additional or checksum algorithms
+        boolean generateAddAlgo = false;
+        if (additionalAlgorithm != null) {
+            FileHashStoreUtility.checkForEmptyAndValidString(
+                additionalAlgorithm, "additionalAlgorithm", "writeToTmpFileAndGenerateChecksums"
+            );
+            validateAlgorithm(additionalAlgorithm);
+            generateAddAlgo = shouldCalculateAlgorithm(additionalAlgorithm);
+        }
+        boolean generateCsAlgo = false;
+        if (checksumAlgorithm != null && !checksumAlgorithm.equals(additionalAlgorithm)) {
+            FileHashStoreUtility.checkForEmptyAndValidString(
+                checksumAlgorithm, "checksumAlgorithm", "writeToTmpFileAndGenerateChecksums"
+            );
+            validateAlgorithm(checksumAlgorithm);
+            generateCsAlgo = shouldCalculateAlgorithm(checksumAlgorithm);
+        }
+
+        MessageDigest md5 = MessageDigest.getInstance(DefaultHashAlgorithms.MD5.getName());
+        MessageDigest sha1 = MessageDigest.getInstance(DefaultHashAlgorithms.SHA_1.getName());
+        MessageDigest sha256 = MessageDigest.getInstance(DefaultHashAlgorithms.SHA_256.getName());
+        MessageDigest sha384 = MessageDigest.getInstance(DefaultHashAlgorithms.SHA_384.getName());
+        MessageDigest sha512 = MessageDigest.getInstance(DefaultHashAlgorithms.SHA_512.getName());
+        MessageDigest additionalAlgo = null;
+        MessageDigest checksumAlgo = null;
+        if (generateAddAlgo) {
+            logFileHashStoreLinks.debug(
+                "Adding additional algorithm to hex digest map, algorithm: " + additionalAlgorithm);
+            additionalAlgo = MessageDigest.getInstance(additionalAlgorithm);
+        }
+        if (generateCsAlgo) {
+            logFileHashStoreLinks.debug(
+                "Adding checksum algorithm to hex digest map, algorithm: " + checksumAlgorithm);
+            checksumAlgo = MessageDigest.getInstance(checksumAlgorithm);
+        }
+
+        // Calculate hex digests
+        try {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = dataStream.read(buffer)) != -1) {
+                md5.update(buffer, 0, bytesRead);
+                sha1.update(buffer, 0, bytesRead);
+                sha256.update(buffer, 0, bytesRead);
+                sha384.update(buffer, 0, bytesRead);
+                sha512.update(buffer, 0, bytesRead);
+                if (generateAddAlgo) {
+                    additionalAlgo.update(buffer, 0, bytesRead);
+                }
+                if (generateCsAlgo) {
+                    checksumAlgo.update(buffer, 0, bytesRead);
+                }
+            }
+
+        } catch (IOException ioe) {
+            String errMsg = "Unexpected Exception ~ " + ioe.getMessage();
+            logFileHashStoreLinks.error(errMsg);
+            throw ioe;
+
+        } finally {
+            dataStream.close();
+        }
+
+        // Create map of hash algorithms and corresponding hex digests
+        Map<String, String> hexDigests = new HashMap<>();
+        String md5Digest = DatatypeConverter.printHexBinary(md5.digest()).toLowerCase();
+        String sha1Digest = DatatypeConverter.printHexBinary(sha1.digest()).toLowerCase();
+        String sha256Digest = DatatypeConverter.printHexBinary(sha256.digest()).toLowerCase();
+        String sha384Digest = DatatypeConverter.printHexBinary(sha384.digest()).toLowerCase();
+        String sha512Digest = DatatypeConverter.printHexBinary(sha512.digest()).toLowerCase();
+        hexDigests.put(DefaultHashAlgorithms.MD5.getName(), md5Digest);
+        hexDigests.put(DefaultHashAlgorithms.SHA_1.getName(), sha1Digest);
+        hexDigests.put(DefaultHashAlgorithms.SHA_256.getName(), sha256Digest);
+        hexDigests.put(DefaultHashAlgorithms.SHA_384.getName(), sha384Digest);
+        hexDigests.put(DefaultHashAlgorithms.SHA_512.getName(), sha512Digest);
+        if (generateAddAlgo) {
+            String extraAlgoDigest = DatatypeConverter.printHexBinary(additionalAlgo.digest())
+                .toLowerCase();
+            hexDigests.put(additionalAlgorithm, extraAlgoDigest);
+        }
+        if (generateCsAlgo) {
+            String extraChecksumDigest = DatatypeConverter.printHexBinary(checksumAlgo.digest())
+                .toLowerCase();
+            hexDigests.put(checksumAlgorithm, extraChecksumDigest);
+        }
+
+        return hexDigests;
     }
 
 }
