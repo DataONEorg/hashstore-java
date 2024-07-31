6f416b37b012a68db8d82587318528d6140d6ac5
Dou Mok <douming.mok@gmail.com>
2024-07-31 15:28:08 -0700

    Refactor 'convert' method to conditionally store the data object or sysmeta, which may not always be available

--- src/main/java/org/dataone/hashstore/hashstoreconverter/HashStoreConverter.java
+++ src/main/java/org/dataone/hashstore/hashstoreconverter/HashStoreConverter.java
@@ -3,6 +3,7 @@ package org.dataone.hashstore.hashstoreconverter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.dataone.hashstore.ObjectMetadata;
+import org.dataone.hashstore.filehashstore.FileHashStoreUtility;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -54,9 +55,12 @@ public class HashStoreConverter {
      * hard link (to save disk space), store the supplied system metadata and return the
      * ObjectMetadata for the data object.
      *
+     * A 'filePath' may be null, in which case a data object will not be stored. The
+     * 'sysMetaStream' may be null, in which case the sysmeta will not be stored.
+     *
      * @param filePath      Path to existing data object
      * @param pid           Persistent or authority-based identifier
-     * @param sysmetaStream Stream to sysmeta content to store
+     * @param sysmetaStream Stream to sysmeta content to store.
      * @return ObjectMetadata for the given pid
      * @throws IOException              An issue with calculating checksums or storing sysmeta
      * @throws NoSuchAlgorithmException An algorithm defined is not supported
@@ -65,31 +69,49 @@ public class HashStoreConverter {
     public ObjectMetadata convert(Path filePath, String pid, InputStream sysmetaStream)
         throws IOException, NoSuchAlgorithmException, InterruptedException {
         logHashStoreConverter.info("Begin converting data object and sysmeta for pid: " + pid);
+        FileHashStoreUtility.checkForEmptyAndValidString(pid, "pid", "convert");
+        boolean storeHardlink = filePath != null;
+        boolean storeSysmeta = sysmetaStream != null;
+        ObjectMetadata objInfo = null;
 
-        try (InputStream fileStream = Files.newInputStream(filePath)) {
-            ObjectMetadata objInfo = fileHashStoreLinks.storeHardLink(filePath, fileStream, pid);
-            fileHashStoreLinks.storeMetadata(sysmetaStream, pid);
-            return objInfo;
+        if (storeHardlink) {
+            try (InputStream fileStream = Files.newInputStream(filePath)) {
+                objInfo = fileHashStoreLinks.storeHardLink(filePath, fileStream, pid);
+                logHashStoreConverter.info("Stored data object for pid: " + pid);
 
-        } catch (IOException ioe) {
-            String errMsg = "Unexpected IOException encountered: " + ioe.getMessage();
-            logHashStoreConverter.error(errMsg);
-            throw ioe;
+            } catch (IOException ioe) {
+                String errMsg = "Unexpected IOException encountered: " + ioe.getMessage();
+                logHashStoreConverter.error(errMsg);
+                throw ioe;
 
-        } catch (NoSuchAlgorithmException nsae) {
-            String errMsg = "A supplied algorithm is not supported: " + nsae.getMessage();
-            logHashStoreConverter.error(errMsg);
-            throw nsae;
+            } catch (NoSuchAlgorithmException nsae) {
+                String errMsg = "A supplied algorithm is not supported: " + nsae.getMessage();
+                logHashStoreConverter.error(errMsg);
+                throw nsae;
 
-        } catch (InterruptedException ie) {
-            String errMsg =
-                "Unexpected issue with synchronizing storing data objects or metadata: "
-                    + ie.getMessage();
-            logHashStoreConverter.error(errMsg);
-            throw ie;
-
-        } finally {
-            sysmetaStream.close();
+            } catch (InterruptedException ie) {
+                String errMsg =
+                    "Unexpected issue with synchronizing storing data objects or metadata: "
+                        + ie.getMessage();
+                logHashStoreConverter.error(errMsg);
+                throw ie;
+            }
+        } else {
+            String warnMsg = "Supplied filePath is null, not storing data object.";
+            logHashStoreConverter.warn(warnMsg);
         }
+
+        if (storeSysmeta) {
+            try {
+                fileHashStoreLinks.storeMetadata(sysmetaStream, pid);
+            } finally {
+                sysmetaStream.close();
+            }
+        } else {
+            String warnMsg = "Supplied sysmeta Stream is null, not storing sysmeta for pid: " + pid;
+            logHashStoreConverter.warn(warnMsg);
+        }
+
+        return objInfo;
     }
 }
