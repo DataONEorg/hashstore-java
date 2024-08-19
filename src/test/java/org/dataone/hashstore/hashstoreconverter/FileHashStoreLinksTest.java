3fcf3e8b25a67c72b43c149c5454cbcc316c3887
Dou Mok <douming.mok@gmail.com>
2024-08-19 10:43:44 -0700

    Revise and add new junit tests related to '...Checksums' methods

--- src/test/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinksTest.java
+++ src/test/java/org/dataone/hashstore/hashstoreconverter/FileHashStoreLinksTest.java
@@ -185,24 +185,43 @@ public class FileHashStoreLinksTest {
             // Get test file
             Path testDataFile = testData.getTestFile(pidFormatted);
 
-            InputStream dataStream = Files.newInputStream(testDataFile);
-            Map<String, String> hexDigests =
-                fileHashStoreLinks.generateChecksums(dataStream, "MD2");
-            dataStream.close();
+            try (InputStream dataStream = Files.newInputStream(testDataFile)) {
+                Map<String, String> hexDigests =
+                    fileHashStoreLinks.generateChecksums(dataStream, "MD2");
 
-            // Validate checksum values
-            String md2 = testData.pidData.get(pid).get("md2");
-            String md5 = testData.pidData.get(pid).get("md5");
-            String sha1 = testData.pidData.get(pid).get("sha1");
-            String sha256 = testData.pidData.get(pid).get("sha256");
-            String sha384 = testData.pidData.get(pid).get("sha384");
-            String sha512 = testData.pidData.get(pid).get("sha512");
-            assertEquals(md2, hexDigests.get("MD2"));
-            assertEquals(md5, hexDigests.get("MD5"));
-            assertEquals(sha1, hexDigests.get("SHA-1"));
-            assertEquals(sha256, hexDigests.get("SHA-256"));
-            assertEquals(sha384, hexDigests.get("SHA-384"));
-            assertEquals(sha512, hexDigests.get("SHA-512"));
+                String md2 = testData.pidData.get(pid).get("md2");
+                String md5 = testData.pidData.get(pid).get("md5");
+                String sha1 = testData.pidData.get(pid).get("sha1");
+                String sha256 = testData.pidData.get(pid).get("sha256");
+                String sha384 = testData.pidData.get(pid).get("sha384");
+                String sha512 = testData.pidData.get(pid).get("sha512");
+                assertEquals(md2, hexDigests.get("MD2"));
+                assertEquals(md5, hexDigests.get("MD5"));
+                assertEquals(sha1, hexDigests.get("SHA-1"));
+                assertEquals(sha256, hexDigests.get("SHA-256"));
+                assertEquals(sha384, hexDigests.get("SHA-384"));
+                assertEquals(sha512, hexDigests.get("SHA-512"));
+
+                assertEquals(hexDigests.size(), 6);
+            }
+        }
+    }
+
+    /**
+     * Confirm that generateChecksums returns the default amount of checksums
+     */
+    @Test
+    public void testGenerateChecksums_defaultChecksumsFound() throws Exception {
+        for (String pid : testData.pidList) {
+            String pidFormatted = pid.replace("/", "_");
+            // Get test file
+            Path testDataFile = testData.getTestFile(pidFormatted);
+
+            try (InputStream dataStream = Files.newInputStream(testDataFile)) {
+                Map<String, String> hexDigests =
+                    fileHashStoreLinks.generateChecksums(dataStream, null);
+                assertEquals(hexDigests.size(), 5);
+            }
         }
     }
 }
