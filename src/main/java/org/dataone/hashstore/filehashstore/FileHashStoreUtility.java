c938a9ba127ee7b06408c188280d44e00d87016d
Dou Mok <douming.mok@gmail.com>
2024-11-07 10:46:29 -0800

    Refactor 'generateTmpFile' in 'FileHashStoreUtility' to set 'rw-r-----' file permissions on tmp file and add new junit test

--- src/main/java/org/dataone/hashstore/filehashstore/FileHashStoreUtility.java
+++ src/main/java/org/dataone/hashstore/filehashstore/FileHashStoreUtility.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,9 +17,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import javax.xml.bind.DatatypeConverter;
@@ -336,10 +339,19 @@ public class FileHashStoreUtility {
         int randomNumber = rand.nextInt(1000000);
         String newPrefix = prefix + "-" + System.currentTimeMillis() + randomNumber;
 
-        Path newPath = Files.createTempFile(directory, newPrefix, null);
-        File newFile = newPath.toFile();
-        newFile.deleteOnExit();
-        return newFile;
+        Path newTmpPath = Files.createTempFile(directory, newPrefix, null);
+        File newTmpFile = newTmpPath.toFile();
+
+        // Set default file permissions 'rw- r-- ---' / posix 640
+        Set<PosixFilePermission> permissions = new HashSet<>();
+        permissions.add(PosixFilePermission.OWNER_READ);
+        permissions.add(PosixFilePermission.OWNER_WRITE);
+        permissions.add(PosixFilePermission.GROUP_READ);
+        Files.setPosixFilePermissions(newTmpPath, permissions);
+        // Mark tmp file to be cleaned up if it runs into an issue
+        newTmpFile.deleteOnExit();
+
+        return newTmpFile;
     }
 
     /**
--- src/test/java/org/dataone/hashstore/filehashstore/FileHashStoreProtectedTest.java
+++ src/test/java/org/dataone/hashstore/filehashstore/FileHashStoreProtectedTest.java
@@ -15,15 +15,18 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -2420,5 +2423,25 @@ public class FileHashStoreProtectedTest {
                 assertTrue(Files.exists(metadataPath));
             }
         }
+    }
+
+
+    /**
+     * Confirm that generateTemporaryFile creates tmpFile with expected permissions
+     */
+    @Test
+    public void fileHashStoreUtility_generateTemporaryFile_permissions() throws Exception {
+        Path directory = tempFolder.resolve("hashstore");
+        // newFile
+        File tmpFile = FileHashStoreUtility.generateTmpFile("testfile", directory);
+
+        Collection<PosixFilePermission> expectedPermissions = new HashSet<>();
+        expectedPermissions.add(PosixFilePermission.OWNER_READ);
+        expectedPermissions.add(PosixFilePermission.OWNER_WRITE);
+        expectedPermissions.add(PosixFilePermission.GROUP_READ);
+
+        Set<PosixFilePermission> actualPermissions = Files.getPosixFilePermissions(tmpFile.toPath());
+
+        assertEquals(expectedPermissions, actualPermissions);
     }
 }
