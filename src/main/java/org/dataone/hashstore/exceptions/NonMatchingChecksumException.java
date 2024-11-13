de06851e56793c612315ec0388b3456679176afa
Dou Mok <douming.mok@gmail.com>
2024-11-13 12:59:30 -0800

    Revise 'NonMatchingChecksumException' class to have a 'hexDigests' attribute

--- src/main/java/org/dataone/hashstore/exceptions/NonMatchingChecksumException.java
+++ src/main/java/org/dataone/hashstore/exceptions/NonMatchingChecksumException.java
@@ -1,13 +1,21 @@
 package org.dataone.hashstore.exceptions;
 
+import java.util.Map;
+
 /**
  * An exception thrown when a checksum does not match what is expected.
  */
 
 public class NonMatchingChecksumException extends IllegalArgumentException {
 
-    public NonMatchingChecksumException(String message) {
+    private final Map<String, String> hexDigests;
+
+    public NonMatchingChecksumException(String message, Map<String, String> checksumMap) {
         super(message);
+        this.hexDigests = checksumMap;
     }
 
+    public Map<String, String> getHexDigests() {
+        return hexDigests;
+    }
 }
